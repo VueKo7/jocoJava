9160c5cc912dfd711c9b7086f916ffeadb25363d
Limone <simonepelle06@gmail.com>
2024-02-08 21:12:39 +0100

    add comment to movement

--- src/entity/Position.java
+++ src/entity/Position.java
@@ -1,10 +1,13 @@
 package entity;
 
+//La classe Position permette di settare le posizioni delle entità e di modificarne le loro posizioni 
+//passandogli le nuove coordinate. Viene usata dalla classe Entity e Movement
 public class Position {
-    
+    //Coordinata delle ascisse
     private int x;
+    //Coordinata delle ordinate
     private int y;
-
+    //Costruttore di default, dove vengono passate le coordinate e salvate
     Position(int x, int y) {
         this.x = x;
         this.y = y;
@@ -12,13 +15,15 @@ public class Position {
 
 
     
-
+    //metodo per la ricezione della coordinata delle ascisse
     public int getX() { return x; }
+    //metodo per il settaggio della coordinata delle ascisse
     public void setX(int x) {
         this.x += x;
     }
-
+    //metodo per la ricezione della coordinata delle ordinate
     public int getY() { return y; }
+    //metodo per il settaggio della coordinata delle ordinate
     public void setY(int y) {
         this.y += y;
     }
